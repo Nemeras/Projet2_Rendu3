9f524f509ce514cbdd8ba7d7eacdb602be8707cb
Nemeras <clementsartori35@gmail.com>
2015-04-07 21:48:09 +0200

    yolo

--- tseitin/_build/form.ml
+++
@@ -1,9 +0,0 @@
-type formula =
-|Lit of int*int
-|And of formula*formula*int
-|Or of formula*formula*int
-|Not of formula*int;;
-
-type form = formula*int;;
-
-type formlist = (formula list) * int
--- tseitin/tseitin.ml
+++ tseitin/tseitin.ml
@@ -5,10 +5,10 @@ let numeroter formula debut =
 	let numerotage = ref (debut) in
 		let rec num form nvar = 
 			match form with
-			|Lit(a,b) -> nvar := !nvar + 1; Lit(a,(!nvar))
-			|And(a,b,c) -> nvar:=1 + !nvar; And(num a nvar, num b nvar, !nvar)
-			|Or(a,b,c) -> nvar := !nvar + 1;  And(num a nvar, num b nvar, !nvar)
-			|Not(a,b) -> nvar := !nvar + 1; Not(num a nvar,!nvar) in
+			|Lit(a,b) -> nvar := !nvar + 1;let temp = !nvar in Lit(a,temp)
+			|And(a,b,c) -> nvar:=1 + !nvar;let temp = !nvar in And(num a nvar, num b nvar, temp)
+			|Or(a,b,c) -> printf "bouh\n";nvar := !nvar + 1;let temp = !nvar in  Or(num a nvar, num b nvar, temp)
+			|Not(a,b) -> nvar := !nvar + 1;let temp = !nvar in Not(num a nvar,temp) in
  			let rec num_aux liste nvar =
 			match liste with
 			|tree::other -> (num tree nvar)::(num_aux other nvar)
@@ -25,7 +25,7 @@ match form with
 let rec print_form tree buffer =
 match tree with
 |Lit(a,b) -> fprintf buffer "-%d %d 0\n%d -%d 0\n" a b a b;
-|And(a,b,c) -> fprintf buffer "-%d %d 0\n%d %d 0\n%d %d %d 0\n" c (etage a) c (etage b) c (etage a) (etage b); print_form a buffer; print_form b buffer;
+|And(a,b,c) -> fprintf buffer "-%d %d 0\n-%d %d 0\n%d %d %d 0\n" c (etage a) c (etage b) c (etage a) (etage b); print_form a buffer; print_form b buffer;
 |Or(a,b,c) -> fprintf buffer "-%d %d %d 0\n%d -%d 0\n%d -%d 0\n" c (etage a) (etage b) c (etage a) c (etage b); print_form a buffer; print_form b buffer;
 |Not(Lit(a,b),c) -> fprintf buffer "-%d -%d 0\n%d %d 0\n" c a c a
 |Not(a,b) ->fprintf buffer "-%d -%d 0\n%d %d 0\n" (etage a) b (etage a) b; print_form a buffer;;
